59ccc7b5b509b893e1afd0df788c114ec57fcc7a
Nikolay <mutovin36@gmail.com>
2026-02-08 22:49:27 +0300

    Initial commit1

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,27 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) {
+                minMonth = i;
+            }
+        }
+        return minMonth + 1;
+    }
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+        return maxMonth + 1;
+    }
+}
+
+
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,29 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class StatsServiceTest {
+    @Test
+    void testMinSales() {
+        StatsService service = new StatsService();
+        long[] minMonth = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 9;
+
+        long actual = service.minSales (minMonth);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void testMaxSales() {
+        StatsService service = new StatsService();
+        long[] maxMonth = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 8;
+
+        long actual = service.maxSales (maxMonth);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+}
